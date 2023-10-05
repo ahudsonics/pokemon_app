f6aa912cdc2b971a585b11c8f89db4e0da72ed83
anthonydhudson <ahudson@icstars.org>
2023-10-05 16:27:21 -0500

    database
    "Connected my database, can see json in the browser"

--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const pokemon = require('./models/pokemon'); // Path may vary based on your file structure
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Pokemon App!');
+});
+
+app.get('/pokemon', (req, res) => {
+  res.json(pokemon);
 });
 
 app.listen(port, () => {
