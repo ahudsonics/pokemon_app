29409255b795f7691223a7e9c5aa5bbf9de91df5
anthonydhudson <ahudson@icstars.org>
2023-10-05 17:10:39 -0500

    database

--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const pokemon = require('./models/pokemon');
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Pokemon App!');
 });
 
+app.get('/pokemon', (req, res) => {
+  res.json(pokemon);
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
